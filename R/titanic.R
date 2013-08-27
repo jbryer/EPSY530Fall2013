e96b3c2e615037e791438d64d3d18b74c721cb95
Jason Bryer <jason@bryer.org>
2013-08-27 14:25:40 -0400

    Updated titanic data.

--- R/titanic.R
+++ R/titanic.R
@@ -1,3 +1,6 @@
+# Downloaded from biostat.mc.vanderbilt.edu/twiki/bin/view/Main/DataSets
+# Documentation: biostat.mc.vanderbilt.edu/wiki/pub/Main/DataSets/Ctitanic3.html
+
 # Read the data
 titanic <- read.csv('Data/titanic.csv', stringsAsFactors=FALSE)
 
@@ -10,6 +13,9 @@ str(titanic)
 # Convert the survived variable to a factor. The data came in as 0 and 1 where
 # 0 = did not survive, 1 = survived. We'll code this as "Yes" and "No", respectively.
 titanic$survived <- factor(titanic$survived, levels=c(0,1), labels=c('No','Yes'))
+titanic$pclass <- factor(titanic$pclass, levels=1:3, 
+						 labels=c('First','Second','Third'), ordered=TRUE)
+save(titanic, file='Data/titanic.Rda')
 
 # Frequency Tables
 table(titanic$survived)
