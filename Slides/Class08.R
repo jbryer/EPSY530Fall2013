385f9f630a1a60bba6cf48c19ede432525e7b550
Jason Bryer <jason@bryer.org>
2013-10-01 10:50:37 -0400

    Fixed typos in slides.

--- Slides/Class08.R
+++ Slides/Class08.R
@@ -1,5 +1,12 @@
 
 
+require(ggplot2)
+require(gdata)
+options(digits=4, width=95)
+opts_chunk$set(fig.path='Figures/Class06-')
+opts_chunk$set(comment=NA)
+source('../R/contingency.table.R')
+
 sat <- read.csv('../Data/Textbook/Chapter_7/SAT_scores.csv', stringsAsFactors=FALSE)
 sat$Verbal.SAT <- as.integer(sat$Verbal.SAT)
 sat$Math.SAT <- as.integer(sat$Math.SAT)
@@ -14,9 +21,11 @@ cor(sat$Math.SAT, sat$Verbal.SAT)
 sat.lm <- lm(Math.SAT ~ Verbal.SAT, data=sat)
 summary(sat.lm)
 ggplot(sat, aes(x=Verbal.SAT, y=Math.SAT)) + 
-	geom_point(color='blue') + 
+	geom_point(color='black') + 
 	geom_abline(slope=sat.lm$coefficients[2], intercept=sat.lm$coefficients[1],
-				color='red')
+				color='red', size=2) +
+	geom_hline(yintercept=mean(sat$Math.SAT), color='blue', size=1) +
+	geom_vline(xintercept=mean(sat$Verbal.SAT), color='blue', size=1)
 
 
 
